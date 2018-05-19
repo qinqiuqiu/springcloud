2ba1ce6c084e3a5d5585f407d95f01ab8b35c15d
qinqiuqiu <qinqiuqiu@gmail.com>
2018-05-19 15:43:40 +0800

    provider-user consumer-movie to consumer user 基于consumer-movie对provider-user服务进行负载均衡 (ribbon  LoadBalance)

---
+++ consumer_movie/src/main/java/org/troy/controller/IndexController.java
@@ -0,0 +1,27 @@
+package org.troy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+/**
+ * @title
+ * @Author 覃球球
+ * @Version 1.0 on 2018/5/19.
+ * @Copyright 湖南科权
+ */
+@RestController
+public class IndexController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/movie")
+    public Object consumer(){
+        return restTemplate.getForObject("http://provider-user-service/hi", Map.class);
+    }
+}
---
+++ provider_user/src/main/java/org/troy/controller/IndexController.java
@@ -0,0 +1,33 @@
+package org.troy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @title
+ * @Author 覃球球
+ * @Version 1.0 on 2018/5/19.
+ * @Copyright 湖南科权
+ */
+@RestController
+public class IndexController {
+
+    @Autowired
+    private Environment env;
+
+    @GetMapping("/hi")
+    public Object hi(){
+        System.out.println("hi， I am " + env.getProperty("spring.application.name")
+                + "  port : " + env.getProperty("server.port"));
+
+        Map<String, String> maps = new HashMap<String, String>();
+        maps.put("name", env.getProperty("spring.application.name"));
+        maps.put("port", env.getProperty("server.port"));
+        return maps;
+    }
+}
