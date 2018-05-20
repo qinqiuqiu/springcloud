0b2995470fe0c09b9863e2eccc4db71ee3014c87
qinqiuqiu <qinqiuqiu@gmail.com>
2018-05-20 13:29:18 +0800

    properties-customizing 	application.ymi配置 		serviceId.ribbon.NFLoadBalancerRuleClassName: com.netflix.loadbalance.RandomRule
    Ribbon Without Eureka
    	application.ymi配置
    		ribbon.eureka.enabled = false  #不启用eureka
    		serviceId.ribbon.listOfServers=host:port # 配置访问微服务的主机地址

---
+++ consumer_movie_ribbon_ext2/src/main/java/org/troy/ribbon/controller/IndexController.java
@@ -0,0 +1,46 @@
+package org.troy.ribbon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @title
+ * @Author 覃球球
+ * @Version 1.0 on 2018/5/19.
+ * @Copyright 湖南科权
+ */
+@RestController
+public class IndexController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("/movie")
+    public Object consumer(){
+        return restTemplate.getForObject("http://provider-user-service/hi", Map.class);
+    }
+
+    @GetMapping("/lb")
+    public Object getLoadBalance(){
+        ServiceInstance s1 = loadBalancerClient.choose("provider-user-service");
+
+        ServiceInstance s2 = loadBalancerClient.choose("provider-user-service2");
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put(s1.getServiceId(), s1.getHost() + ":" + s1.getPort());
+        map.put(s2.getServiceId(), s2.getHost() + ":" + s2.getPort());
+
+        System.out.println("map ======================> " + map);
+        return map;
+    }
+}
